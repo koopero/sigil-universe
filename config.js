a6e28787a47efedb4d566554888cb3e674aee508
Samm Cooper <pi@localhost.localdomain>
2013-05-28 23:32:37 -0700

    Fixed config.js

--- config.js
+++ config.js
@@ -1,2 +1,16 @@
-var configContents = fs.readFileSync( 'config.json','utf8'); 
-exports = JSON.parse(configContents); 
+var path = require('path');
+var fs = require('fs');
+
+var configFile = path.join ( path.dirname( __filename), "config.json" );
+var configContents = fs.readFileSync( configFile,'utf8'); 
+extend ( exports, JSON.parse(configContents) );
+
+function extend(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+}
